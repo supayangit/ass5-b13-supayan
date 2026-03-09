676ef84ee62dd35909f22860e2485b5e2eb3211f
supayangit <supayanchk@gmail.com>
2026-03-09 16:24:25 +0600

    final push - page directory fixed

--- script/index.js
+++ script/index.js
@@ -1,16 +1,19 @@
 const doSignIn = () => {
-    const userNameField = document.getElementById("username-input");
-    const passwordField = document.getElementById("password-input");
- const userName = userNameField.value;
- const password = passwordField.value;
+   const userNameField = document.getElementById("username-input");
+   const passwordField = document.getElementById("password-input");
 
- if(userName == "admin" && password == "admin123"){
-    alert("Sign In Successfull!");
-    window.location.href = "/dashboard.html";
- } else {
-    alert("Enter Correct Credentials!");
-    userNameField.value= "";
-    passwordField.value= "";
-    return;
- }
+   // credentials
+   const userName = userNameField.value;
+   const password = passwordField.value;
+   const defUsername = document.getElementById("default-username").innerText;
+   const defPassword = document.getElementById("default-password").innerText;
+   if (userName == defUsername && password == defPassword) {
+      alert("Sign In Successfull!");
+      window.location.href = "dashboard.html";
+   } else {
+      alert("Enter Correct Credentials!");
+      userNameField.value = "";
+      passwordField.value = "";
+      return;
+   }
 };
